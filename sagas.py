f1de12e47fec3fbacc9aaff0f57e4a541be477e1
figarocorso <figarocorso@gmail.com>
2024-11-01 23:51:11 +0100

    Add LoTR, Hobbit and Harry Potter sagas

--- sagas.py
+++ sagas.py
@@ -1,30 +1,64 @@
 SAGAS = {
     "Lord of the Rings": [
         {
-            "id": "tt0032138",
-            "name": "The Wizard of Oz",
+            "id": "tt0120737",
+            "name": "The Fellowship of the Ring",
         },
         {
-            "id": "tt0017136",
-            "name": "Metropolis",
+            "id": "tt0167261",
+            "name": "The Two Towers",
         },
         {
-            "id": "tt0051744",
-            "name": "House on Haunted Hill",
+            "id": "tt0167260",
+            "name": "The Return of the King",
+        },
+    ],
+    "The Hobbit": [
+        {
+            "id": "tt0903624",
+            "name": "An Unexpected Journey",
+        },
+        {
+            "id": "tt1170358",
+            "name": "The Desolation of Smaug",
+        },
+        {
+            "id": "tt2310332",
+            "name": "The Battle of the Five Armies",
         },
     ],
     "Harry Potter": [
         {
-            "id": "tt1254207",
-            "name": "Big Buck Bunny",
+            "id": "tt0241527",
+            "name": "Harry Potter and the Sorcerer's Stone",
         },
         {
-            "id": "tt0031051",
-            "name": "The Arizona Kid",
+            "id": "tt0295297",
+            "name": "Harry Potter and the Chamber of Secrets",
         },
         {
-            "id": "tt0137523",
-            "name": "Fight Club",
+            "id": "tt0304141",
+            "name": "Harry Potter and the Prisoner of Azkaban",
+        },
+        {
+            "id": "tt0330373",
+            "name": "Harry Potter and the Goblet of Fire",
+        },
+        {
+            "id": "tt0373889",
+            "name": "Harry Potter and the Order of the Phoenix",
+        },
+        {
+            "id": "tt0417741",
+            "name": "Harry Potter and the Half-Blood Prince",
+        },
+        {
+            "id": "tt0926084",
+            "name": "Harry Potter and the Deathly Hallows: Part 1",
+        },
+        {
+            "id": "tt1201607",
+            "name": "Harry Potter and the Deathly Hallows: Part 2",
         },
     ],
 }
